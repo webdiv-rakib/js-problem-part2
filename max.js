ab78669154ee283ddb30075081d994d888a0d113
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-14 20:59:00 +0600

    max number between 2 numbers

--- max.js
+++ max.js
@@ -6,3 +6,14 @@ if (disha > salman) {
 else {
     console.log("Salman will get the strawberry");
 }
+// Inside a function
+function getMax(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+const max = getMax(5, 7);
+console.log('max of two is:', max);
