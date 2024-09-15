cf9c25f921d12ce62399cdfdf310fac9c47f7570
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-16 01:13:18 +0600

    find out cheapest price phone from an array

---
+++ min.js
@@ -0,0 +1,13 @@
+const price = [20000, 16000, 50000, 10000, 12000, 30000, 35000];
+
+function getMin(numbers) {
+    let min = numbers[0];
+    for (const num of numbers) {
+        if (num < min) {
+            min = num;
+        }
+    }
+    return min;
+}
+const cheap = getMin(price);
+console.log("cheapest one is: ", cheap);
