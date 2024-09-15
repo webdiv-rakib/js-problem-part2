75f1333b51ac54d0cf3370dd1d611648052af426
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-16 01:04:31 +0600

    home work of product price

--- wood.js
+++ wood.js
@@ -3,17 +3,38 @@
  * table ---> 10 cft
  * bed ---> 50 cft
  */
-function woodQuantity(chairQuantity, tableQuantity, bedQuantity) {
-    const perChairWood = 3;
-    const perTableWood = 10;
-    const perBedWood = 50;
+// function woodQuantity(chairQuantity, tableQuantity, bedQuantity) {
+//     const perChairWood = 3;
+//     const perTableWood = 10;
+//     const perBedWood = 50;
 
-    const chairTotalWood = chairQuantity * perChairWood;
-    const tableTotalWood = tableQuantity * perTableWood;
-    const bedTotalWood = bedQuantity * perBedWood;
+//     const chairTotalWood = chairQuantity * perChairWood;
+//     const tableTotalWood = tableQuantity * perTableWood;
+//     const bedTotalWood = bedQuantity * perBedWood;
 
-    const totalWood = chairTotalWood + tableTotalWood + bedTotalWood;
-    return totalWood;
+//     const totalWood = chairTotalWood + tableTotalWood + bedTotalWood;
+//     return totalWood;
+// }
+// const wood = woodQuantity(5, 3, 1);
+// console.log("Wood Needed:", wood);
+
+/**
+ * Shirt price example
+ * shirt price ---> 500
+ * pant price ---> 300
+ * shoe price ---> 900
+ */
+function productQuantity(shirtQuantity, pantQuantity, shoeQuantity) {
+    const perShirtPrice = 500;
+    const perPantPrice = 300;
+    const perShoePrice = 900;
+
+    const shirtTotalPrice = shirtQuantity * perShirtPrice;
+    const pantTotalPrice = pantQuantity * perPantPrice;
+    const shoeTotalPrice = shoeQuantity * perShoePrice;
+
+    const totalCost = shirtTotalPrice + pantTotalPrice + shoeTotalPrice;
+    return totalCost
 }
-const wood = woodQuantity(5, 3, 1);
-console.log("Wood Needed:", wood);
+const totalProductPrice = productQuantity(4, 5, 6);
+console.log("Total Cost: ", totalProductPrice);
