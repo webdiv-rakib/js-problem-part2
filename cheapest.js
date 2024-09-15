b23921caa8d3b6486c94f038600e8d857f2b210a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-16 01:58:55 +0600

    finding out cheapest phone from an array

---
+++ cheapest.js
@@ -0,0 +1,21 @@
+const mobile = [
+    { name: 'Samsung', price: 20000, camera: '12mp', color: 'black' },
+    { name: 'Xiaomi', price: 16000, camera: '12mp', color: 'black' },
+    { name: 'Oppo', price: 30000, camera: '12mp', color: 'black' },
+    { name: 'Iphone', price: 100000, camera: '12mp', color: 'black' },
+    { name: 'Walton', price: 12000, camera: '12mp', color: 'black' },
+    { name: 'HTC', price: 25000, camera: '12mp', color: 'black' }
+]
+
+function getCheapestPhone(phones) {
+    let min = phones[0];
+    for (const phone of phones) {
+        // console.log(phone);
+        if (phone.price < min.price) {
+            min = phone;
+        }
+    }
+    return min;
+}
+const cheap = getCheapestPhone(mobile);
+console.log(cheap);
